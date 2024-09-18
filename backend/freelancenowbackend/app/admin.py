f6580d362286f243692534180bea7c919fa47e00
Sara Lucia <saludipu71@gmail.com>
2024-09-18 14:22:39 -0500

    fix: data model

--- backend/freelancenowbackend/app/admin.py
+++ backend/freelancenowbackend/app/admin.py
@@ -1,18 +1,22 @@
 from django.contrib import admin
-from .models import User, Company, Area, Project, Freelancer, Milestone, Deliverable, Role, Permission, Notification, UserRole, ProjectFreelancer, UserNotification
+from .models import ProjectSkill, Payment, Comment, Experience, FreelancerSkill, Portfolio, User, Company, Area, Project, Freelancer, Milestone, Deliverable, Role, Permission, Notification, UserRole, ProjectFreelancer, UserNotification
 
 admin.site.register(User)
-admin.site.register(Company)
-admin.site.register(Area)
-admin.site.register(Project)
-admin.site.register(Freelancer)
-admin.site.register(Milestone)
-admin.site.register(Deliverable)
 admin.site.register(Role)
+admin.site.register(UserRole)
 admin.site.register(Permission)
 admin.site.register(Notification)
-admin.site.register(UserRole)
-admin.site.register(ProjectFreelancer)
 admin.site.register(UserNotification)
-
-
+admin.site.register(Company)
+admin.site.register(Area)
+admin.site.register(Freelancer)
+admin.site.register(FreelancerSkill)
+admin.site.register(Experience)
+admin.site.register(Portfolio)
+admin.site.register(Comment)
+admin.site.register(Project)
+admin.site.register(ProjectFreelancer)
+admin.site.register(ProjectSkill)
+admin.site.register(Milestone)
+admin.site.register(Deliverable)
+admin.site.register(Payment)
