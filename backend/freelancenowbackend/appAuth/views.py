cf329df0d9f3882b38c6a4ffa0e06c0e0d3cc092
Sara Lucia <saludipu71@gmail.com>
2024-09-19 01:44:58 -0500

    fix: login

--- backend/freelancenowbackend/appAuth/views.py
+++ backend/freelancenowbackend/appAuth/views.py
@@ -1,12 +1,14 @@
-from rest_framework import generics
+from rest_framework import generics, status
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
-from rest_framework import status
 from django.contrib.auth import authenticate
-from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
+from rest_framework.views import APIView
 from app.models import User, Role, UserRole
 from .serializers import UserSerializer
+import logging
+
+logger = logging.getLogger(__name__)
 
 class FreelancerRegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
@@ -15,12 +17,11 @@ class FreelancerRegisterView(generics.CreateAPIView):
 
     def perform_create(self, serializer):
         user = serializer.save()  # Guarda el usuario
-        user.set_password(user.password)  # Aplica el hash a la contraseña
-        user.save()  # Guarda el usuario nuevamente con la contraseña encriptada
-        
+        # La contraseña ya se maneja en el serializador
         # Asigna el rol de Freelancer
         role, created = Role.objects.get_or_create(name='Freelancer')
         UserRole.objects.create(user=user, role=role)
+
 
 class BusinessManagerRegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
@@ -29,12 +30,11 @@ class BusinessManagerRegisterView(generics.CreateAPIView):
 
     def perform_create(self, serializer):
         user = serializer.save()  # Guarda el usuario
-        user.set_password(user.password)  # Aplica el hash a la contraseña
-        user.save()  # Guarda el usuario nuevamente con la contraseña encriptada
-        
+        # La contraseña ya se maneja en el serializador
         # Asigna el rol de Business Manager
         role, created = Role.objects.get_or_create(name='Business Manager')
         UserRole.objects.create(user=user, role=role)
+
 
 class RegisterCompanyUsersView(generics.CreateAPIView):
     queryset = User.objects.all()
@@ -46,38 +46,42 @@ class RegisterCompanyUsersView(generics.CreateAPIView):
 
         # Validar que sea un rol permitido
         if role_name not in ['Area Admin', 'Project Manager']:
-            return Response({"error": "Invalid role"}, status=status.HTTP_400_BAD_REQUEST)
+            raise serializers.ValidationError({"error": "Invalid role"})
 
         # Asegurarse que el que hace la solicitud es un Business Manager
         if not UserRole.objects.filter(user=self.request.user, role__name='Business Manager').exists():
-            return Response({"error": "Unauthorized"}, status=status.HTTP_403_FORBIDDEN)
+            raise serializers.ValidationError({"error": "Unauthorized"}, code=403)
 
         # Crea el usuario y asocia el rol
         user = serializer.save()  # Guarda el usuario
-        user.set_password(user.password)  # Aplica el hash a la contraseña
-        user.save()  # Guarda el usuario nuevamente con la contraseña encriptada
-
         role, created = Role.objects.get_or_create(name=role_name)
         UserRole.objects.create(user=user, role=role)
 
-import logging
 
-logger = logging.getLogger(__name__)
+class LoginView(APIView):
+    permission_classes = [AllowAny]
 
-class CustomAuthToken(ObtainAuthToken):
     def post(self, request, *args, **kwargs):
-        logger.info(f"Request data: {request.data}")
-        print(f"Request data: {request.data}")
-        username = request.data.get('username')
+        logger.debug(f"Request data: {request.data}")
+        email = request.data.get('email')
         password = request.data.get('password')
 
-        if not username or not password:
-            return Response({"non_field_errors": ["Both username and password are required."]}, status=400)
+        if not email or not password:
+            return Response(
+                {'error': 'Both email and password are needed.'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
 
-        user = authenticate(username=username, password=password)
-
+        user = authenticate(request, email=email, password=password)
         if user is None:
-            return Response({"non_field_errors": ["Unable to log in with provided credentials."]}, status=400)
+            return Response(
+                {'error': 'The credentials you entered are invalid.'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
 
         token, created = Token.objects.get_or_create(user=user)
-        return Response({"token": token.key, "user_id": user.id, "email": user.email})
+        return Response({
+            'token': token.key,
+            'user_id': user.id,
+            'email': user.email
+        }, status=status.HTTP_200_OK)
