cf9c5f07b885bbf0433dd1d30980a65d41a85869
Ricardo965 <ricardiox2000@gmail.com>
2024-09-21 20:27:57 -0500

    build: Login and register

---
+++ frontend/src/data/freelancers-data.js
@@ -0,0 +1,50 @@
+export const freelancersData = [
+  {
+    img: "/img/team-1.jpeg",
+    name: "Sophie B.",
+    message: "Frontend developer experienced with Vue.js and Angular.",
+  },
+  {
+    img: "/img/team-2.jpeg",
+    name: "Alexander",
+    message: "Full-stack developer specializing in Node.js and React.",
+  },
+  {
+    img: "/img/team-3.jpeg",
+    name: "Ivanna",
+    message: "UI/UX designer with a passion for creating intuitive interfaces.",
+  },
+  {
+    img: "/img/team-4.jpeg",
+    name: "Peterson",
+    message: "Backend developer skilled in Python, Django, and PostgreSQL.",
+  },
+  {
+    img: "/img/bruce-mars.jpeg",
+    name: "Bruce Mars",
+    message: "Mobile developer proficient in Flutter and React Native.",
+  },
+  {
+    img: "/img/team-3.jpeg",
+    name: "Kevin Nieto",
+    message: "Mobile developer proficient in Kptlin and Jetpack Compose.",
+  },
+  {
+    img: "/img/team-2.jpeg",
+    name: "Alejandro Cordoba",
+    message: "Backend developer skilled in Python, Django, and PostgreSQL.",
+  },
+  {
+    img: "/img/team-3.jpeg",
+    name: "",
+    message: "Frontend developer skilled in React, Vue, and HTML.",
+  },
+  {
+    img: "/img/team-.jpeg",
+    name: "Bruce Mars",
+    message: "Backend developer skilled in Python, Django, and PostgreSQL.",
+  },
+];
+
+
+export default freelancersData;
--- frontend/src/data/index.js
+++ frontend/src/data/index.js
@@ -1,3 +1,4 @@
 export * from "@/data/projects-data";
 export * from "@/data/worker-data";
+export * from "@/data/freelancers-data";
 
