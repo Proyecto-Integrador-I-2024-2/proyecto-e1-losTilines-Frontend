8f13845e5a1defd16efe1a5dabd38d8670fd0782
Ricardo965 <ricardiox2000@gmail.com>
2024-09-22 08:38:30 -0500

    Build: skills needed component, project edition

--- frontend/src/pages/project/projectDetail.jsx
+++ frontend/src/pages/project/projectDetail.jsx
@@ -123,104 +123,19 @@ export function ProjectDetail() {
                   </IconButton>
                 </MenuHandler>
                 <MenuList>
-                  <MenuItem>Action</MenuItem>
+                  <MenuItem>Edit project information</MenuItem>
                   <MenuItem>Another Action</MenuItem>
                   <MenuItem>Something else here</MenuItem>
                 </MenuList>
               </Menu>
             </CardHeader>
             <CardBody className="overflow-x-scroll px-0 pt-0 pb-2">
-              <table className="w-full min-w-[640px] table-auto">
-                <thead>
-                  <tr>
-                    {["companies", "members", "budget", "completion"].map(
-                      (el) => (
-                        <th
-                          key={el}
-                          className="border-b border-blue-gray-50 py-3 px-6 text-left"
-                        >
-                          <Typography
-                            variant="small"
-                            className="text-[11px] font-medium uppercase text-blue-gray-400"
-                          >
-                            {el}
-                          </Typography>
-                        </th>
-                      )
-                    )}
-                  </tr>
-                </thead>
-                <tbody>
-                  {projectsTableData.map(
-                    ({ img, name, members, budget, completion }, key) => {
-                      const className = `py-3 px-5 ${key === projectsTableData.length - 1
-                        ? ""
-                        : "border-b border-blue-gray-50"
-                        }`;
 
-                      return (
-                        <tr key={name}>
-                          <td className={className}>
-                            <div className="flex items-center gap-4">
-                              <Avatar src={img} alt={name} size="sm" />
-                              <Typography
-                                variant="small"
-                                color="blue-gray"
-                                className="font-bold"
-                              >
-                                {name}
-                              </Typography>
-                            </div>
-                          </td>
-                          <td className={className}>
-                            {members.map(({ img, name }, key) => (
-                              <Tooltip key={name} content={name}>
-                                <Avatar
-                                  src={img}
-                                  alt={name}
-                                  size="xs"
-                                  variant="circular"
-                                  className={`cursor-pointer border-2 border-white ${key === 0 ? "" : "-ml-2.5"
-                                    }`}
-                                />
-                              </Tooltip>
-                            ))}
-                          </td>
-                          <td className={className}>
-                            <Typography
-                              variant="small"
-                              className="text-xs font-medium text-blue-gray-600"
-                            >
-                              {budget}
-                            </Typography>
-                          </td>
-                          <td className={className}>
-                            <div className="w-10/12">
-                              <Typography
-                                variant="small"
-                                className="mb-1 block text-xs font-medium text-blue-gray-600"
-                              >
-                                {completion}%
-                              </Typography>
-                              <Progress
-                                value={completion}
-                                variant="gradient"
-                                color={completion === 100 ? "green" : "blue"}
-                                className="h-1"
-                              />
-                            </div>
-                          </td>
-                        </tr>
-                      );
-                    }
-                  )}
-                </tbody>
-              </table>
             </CardBody>
           </Card>
           <Card className="border border-blue-gray-100 shadow-sm max-h-full">
-            <CardBody className="my-4  pt-0 h-128">
-              <SkillsSection sectionName={"Nice to have"} />
+            <CardBody className="my-4  pt-0 h-116">
+              <SkillsSection sectionName={"Skills needed"} />
             </CardBody>
           </Card>
         </div>
