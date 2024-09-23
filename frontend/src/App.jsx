a99a79a31ba5c08ea0a4cd7d4bcfd4e2877eed54
kira0826 <curaca492@gmail.com>
2024-09-23 11:26:35 -0500

    refactor: setting up React Query and axios

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,28 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import { DashboardLayout } from "./layouts/dashboardLayout";
 import { ProfileLayout, Auth, ProjectLayout } from "./layouts";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 60 * 1000,
+    },
+  },
+});
 
 function App() {
   return (
-    <Routes>
-      <Route path="/dashboard/*" element={<DashboardLayout />} />
-      <Route path="/profile/*" element={<ProfileLayout />} />
-      <Route path="/auth/*" element={<Auth />} />
-      <Route path="/project/*" element={<ProjectLayout />} />
-    </Routes>
+    <QueryClientProvider client= {queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <Routes>
+        <Route path="/dashboard/*" element={<DashboardLayout />} />
+        <Route path="/profile/*" element={<ProfileLayout />} />
+        <Route path="/auth/*" element={<Auth />} />
+        <Route path="/project/*" element={<ProjectLayout />} />
+      </Routes>
+    </QueryClientProvider>
   );
 }
 
--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -36,7 +36,7 @@ export const routes = [
   },
   {
     layout: "auth",
-    pages: [
+    pages: [    
       {
         name: "sign in",
         path: "/sign-in",
