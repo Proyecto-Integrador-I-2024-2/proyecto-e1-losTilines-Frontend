e79f0806bd1b191a06cfc447896940c3dd98874d
kira0826 <curaca492@gmail.com>
2024-09-24 03:20:18 -0500

    feat: register sender logic done.

--- frontend/src/layouts/homepageLayout.jsx
+++ frontend/src/layouts/homepageLayout.jsx
@@ -19,3 +19,4 @@ export function homepageLayout() {
     </div>
   );
 }
+export default homepageLayout;
--- frontend/src/layouts/index.js
+++ frontend/src/layouts/index.js
@@ -2,3 +2,5 @@ export * from "@/pages/dashboard/dashboard";
 export * from "@/layouts/profileLayout"
 export * from "@/layouts/projectLayout"
 export * from "@/layouts/auth";
+export * from "@/layouts/homepageLayout";
+    
