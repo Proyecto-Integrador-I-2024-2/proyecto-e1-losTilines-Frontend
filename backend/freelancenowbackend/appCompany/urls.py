470e2671b28004589956d9453ef256a21fd485bc
Sara Lucia <saludipu71@gmail.com>
2024-09-23 22:36:01 -0500

    feat: crud workers by business manager

--- backend/freelancenowbackend/appCompany/urls.py
+++ backend/freelancenowbackend/appCompany/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import WorkingAreaListView, WorkingAreaDetailView, WorkingAreaCreationView, WorkingAreaUpdateView, WorkingAreaDeleteView, ListCompanyWorkersView
+from .views import WorkingAreaListView, WorkingAreaDetailView, WorkingAreaCreationView, WorkingAreaUpdateView, WorkingAreaDeleteView, ListCompanyWorkersView, RetrieveWorkerView, UpdateWorkerView, DeleteWorkerView
 
 urlpatterns = [
     path('areas/', WorkingAreaListView.as_view(), name='working-area-list'),
@@ -7,5 +7,8 @@ urlpatterns = [
     path('areas/create/', WorkingAreaCreationView.as_view(), name='working-area-create'),
     path('areas/update/<int:pk>/', WorkingAreaUpdateView.as_view(), name='working-area-update'),
     path('areas/delete/<int:pk>/', WorkingAreaDeleteView.as_view(), name='working-area-delete'),
-    path('workers/', ListCompanyWorkersView.as_view(), name='workers-list')
+    path('workers/', ListCompanyWorkersView.as_view(), name='workers-list'),
+    path('workers/<int:pk>/', RetrieveWorkerView.as_view(), name='retrieve-worker'),
+    path('workers/update/<int:pk>/', UpdateWorkerView.as_view(), name='update-worker'),
+    path('workers/delete/<int:pk>/', DeleteWorkerView.as_view(), name='delete-worker'),
 ]
