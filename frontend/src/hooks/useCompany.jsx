2f168eeeaa9c3dc16ca2e1fb22fcd8149e925911
kira0826 <curaca492@gmail.com>
2024-09-23 22:17:19 -0500

    feat: adding company info getting

---
+++ frontend/src/hooks/useCompany.jsx
@@ -0,0 +1,19 @@
+import { useQuery } from "@tanstack/react-query";
+import apiClient from "@/services/apiClient";
+
+const fetchCompany  = async () =>{
+
+    const {data}  = await apiClient.get("/profile/info/");
+    return data;
+}
+
+export const useUser = () => {
+
+    return useQuery(['User'], fetchUser, {
+        staleTime: 1000 * 60 * 3,
+        cachetime: 1000 * 60 * 30,
+        retry: 2
+    })
+}
+
+export default useUser;
