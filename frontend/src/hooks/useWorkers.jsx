31557f0176cf7ab0aca0a3ea5966b753dc9d29f8
Raul Quigua <raul-quigua@hotmail.com>
2024-09-23 21:51:40 -0500

    feat: dashboard landing

--- frontend/src/hooks/useWorkers.jsx
+++ frontend/src/hooks/useWorkers.jsx
@@ -1,4 +1,4 @@
-import { useQuery } from 'react-query';
+import { useQuery } from '@tanstack/react-query';
 import apiClient from '../services/apiClient'; // Asegúrate de que esta ruta sea correcta
 
 const fetchWorkers = async () => {
