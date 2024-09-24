0cb9e1db6b56282ceb3d46f1e2b0e698580c2179
Raul Quigua <raul-quigua@hotmail.com>
2024-09-24 03:05:30 -0500

    feat: homepage layout and page done

--- frontend/src/layouts/index.js
+++ frontend/src/layouts/index.js
@@ -2,3 +2,6 @@ export * from "@/pages/dashboard/dashboard";
 export * from "@/layouts/profileLayout"
 export * from "@/layouts/projectLayout"
 export * from "@/layouts/auth";
+export * from "@/layouts/homepageLayout";
+
+
