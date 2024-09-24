bc8cdd8b0a1b5745c309419b16aee32fad971d36
Raul Quigua <raul-quigua@hotmail.com>
2024-09-23 21:02:49 -0500

    feat: hook for register

---
+++ frontend/src/hooks/useRegister.jsx
@@ -0,0 +1,19 @@
+import { useMutation } from "@tanstack/react-query";
+import apiClient from "../services/apiClient"; 
+
+const registerUser = async (userData) => {
+  const response = await apiClient.post("/users/register/freelancer", userData);
+  return response.data;
+};
+
+export const useRegister = () => {
+  return useMutation(registerUser, {
+    onSuccess: (data) => {
+      console.log("Registration successful:", data);
+    },
+    onError: (error) => {
+      console.error("Registration failed:", error);
+      throw error; 
+    },
+  });
+};
