698976407d2efb55cbb1c5b4ce5d98a375ec53a7
kira0826 <curaca492@gmail.com>
2024-09-24 21:34:51 -0500

    hotfix: fixing superadmin login problem.

--- backend/freelancenowbackend/app/management/commands/init_data.py
+++ backend/freelancenowbackend/app/management/commands/init_data.py
@@ -19,36 +19,4 @@ class Command(BaseCommand):
     help = 'Carga datos iniciales para pruebas.'
 
     def handle(self, *args, **options):
-        try:
-            # Limpiar datos existentes (opcional, ten cuidado con esto en entornos de producción)
-            User.objects.all().delete()
-            Group.objects.all().delete()
-            Notification.objects.all().delete()
-            UserNotification.objects.all().delete()
-            Company.objects.all().delete()
-            Area.objects.all().delete()
-            UserCompany.objects.all().delete()
-            Freelancer.objects.all().delete()
-            SkillType.objects.all().delete()
-            Skill.objects.all().delete()
-            FreelancerSkill.objects.all().delete()
-            Experience.objects.all().delete()
-            Portfolio.objects.all().delete()
-            Comment.objects.all().delete()
-            ProjectStatus.objects.all().delete()
-            Project.objects.all().delete()
-            ProjectFreelancer.objects.all().delete()
-            ProjectSkill.objects.all().delete()
-            Milestone.objects.all().delete()
-            Deliverable.objects.all().delete()
-            Payment.objects.all().delete()
-            Country.objects.all().delete()
-            City.objects.all().delete()
-
-
-        except IntegrityError as e:
-            self.stderr.write(self.style.ERROR(f'Error de integridad: {e}'))
-        except ValidationError as e:
-            self.stderr.write(self.style.ERROR(f'Error de validación: {e}'))
-        except Exception as e:
-            self.stderr.write(self.style.ERROR(f'Ocurrió un error: {e}'))
+        pass
