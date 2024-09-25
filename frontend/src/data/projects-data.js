fdc6ac2bd183633aa5dc0775179702a414d141aa
Ricardo965 <ricardiox2000@gmail.com>
2024-09-25 18:05:56 -0500

    feat: homepage

--- frontend/src/data/projects-data.js
+++ frontend/src/data/projects-data.js
@@ -1,18 +1,20 @@
 export const projectsData = [
+  // {
+  //   id: 1,
+  //   img: "https://images.unsplash.com/photo-1581091870621-1a7f1a1f1a1f",
+  //   title: "AI Management Platform",
+  //   tag: "Project #1",
+  //   description:
+  //     "Desarrollo de una plataforma impulsada por IA para optimizar la gestión interna y mejorar la eficiencia operativa.",
+  //   members: [
+  //     { img: "/img/team-1.jpeg", name: "Carlos Pérez" },
+  //     { img: "/img/team-2.jpeg", name: "Ana Gómez" },
+  //     { img: "/img/team-3.jpeg", name: "Luis Martínez" },
+  //     { img: "/img/team-4.jpeg", name: "María Rodríguez" },
+  //   ],
+  // },
   {
-    img: "https://th.bing.com/th/id/R.8741f4f79e1a7ef4379b5252100721eb?rik=aSf09oM1gBBYmw&pid=ImgRaw&r=0",
-    title: "AI Management Platform",
-    tag: "Project #1",
-    description:
-      "Desarrollo de una plataforma impulsada por IA para optimizar la gestión interna y mejorar la eficiencia operativa.",
-    members: [
-      { img: "/img/team-1.jpeg", name: "Carlos Pérez" },
-      { img: "/img/team-2.jpeg", name: "Ana Gómez" },
-      { img: "/img/team-3.jpeg", name: "Luis Martínez" },
-      { img: "/img/team-4.jpeg", name: "María Rodríguez" },
-    ],
-  },
-  {
+    id: 2,
     img: "https://images.unsplash.com/photo-1518770660439-4636190af475",
     title: "Music Recommendation System",
     tag: "Project #2",
@@ -26,6 +28,7 @@ export const projectsData = [
     ],
   },
   {
+    id: 3,
     img: "https://images.unsplash.com/photo-1519389950473-47ba0277781c",
     title: "Agile Project Management Tool",
     tag: "Project #3",
@@ -39,6 +42,7 @@ export const projectsData = [
     ],
   },
   {
+    id: 4,
     img: "https://images.unsplash.com/photo-1517433456452-f9633a875f6f",
     title: "Color Scheme Generator",
     tag: "Project #4",
@@ -51,20 +55,22 @@ export const projectsData = [
       { img: "/img/team-1.jpeg", name: "Carlos Pérez" },
     ],
   },
+  // {
+  //   id: 5,
+  //   img: "https://images.unsplash.com/photo-1517430816045-df4b7de1a3c0",
+  //   title: "Dark-Themed Code Editor",
+  //   tag: "Project #5",
+  //   description:
+  //     "Editor de código con tema oscuro y resaltado de sintaxis avanzado.",
+  //   members: [
+  //     { img: "/img/team-4.jpeg", name: "María Rodríguez" },
+  //     { img: "/img/team-3.jpeg", name: "Luis Martínez" },
+  //     { img: "/img/team-2.jpeg", name: "Ana Gómez" },
+  //     { img: "/img/team-1.jpeg", name: "Carlos Pérez" },
+  //   ],
+  // },
   {
-    img: "https://images.unsplash.com/photo-1556761175-4b46a572b786",
-    title: "Dark-Themed Code Editor",
-    tag: "Project #5",
-    description:
-      "Editor de código con tema oscuro y resaltado de sintaxis avanzado.",
-    members: [
-      { img: "/img/team-4.jpeg", name: "María Rodríguez" },
-      { img: "/img/team-3.jpeg", name: "Luis Martínez" },
-      { img: "/img/team-2.jpeg", name: "Ana Gómez" },
-      { img: "/img/team-1.jpeg", name: "Carlos Pérez" },
-    ],
-  },
-  {
+    id: 6,
     img: "https://images.unsplash.com/photo-1518770660439-4636190af475",
     title: "Customizable Code Editor",
     tag: "Project #6",
@@ -78,6 +84,7 @@ export const projectsData = [
     ],
   },
   {
+    id: 7,
     img: "https://images.unsplash.com/photo-1556761175-4b46a572b786",
     title: "E-Learning Platform",
     tag: "Project #7",
@@ -91,37 +98,12 @@ export const projectsData = [
     ],
   },
   {
+    id: 8,
     img: "https://images.unsplash.com/photo-1517433456452-f9633a875f6f",
     title: "Smart Security System",
     tag: "Project #8",
     description:
       "Sistema de seguridad inteligente con monitoreo en tiempo real y alertas automáticas.",
-    members: [
-      { img: "/img/team-4.jpeg", name: "María Rodríguez" },
-      { img: "/img/team-3.jpeg", name: "Luis Martínez" },
-      { img: "/img/team-2.jpeg", name: "Ana Gómez" },
-      { img: "/img/team-1.jpeg", name: "Carlos Pérez" },
-    ],
-  },
-  {
-    img: "https://th.bing.com/th/id/R.732084db8006e384efb603cbeb233c88?rik=iPd2Pzgw30q9eg&pid=ImgRaw&r=0",
-    title: "Blockchain Payment System",
-    tag: "Project #9",
-    description:
-      "Sistema de pagos basado en blockchain para transacciones seguras y rápidas.",
-    members: [
-      { img: "/img/team-1.jpeg", name: "Carlos Pérez" },
-      { img: "/img/team-2.jpeg", name: "Ana Gómez" },
-      { img: "/img/team-3.jpeg", name: "Luis Martínez" },
-      { img: "/img/team-4.jpeg", name: "María Rodríguez" },
-    ],
-  },
-  {
-    img: "https://images.unsplash.com/photo-1519389950473-47ba0277781c",
-    title: "IoT Home Automation",
-    tag: "Project #10",
-    description:
-      "Sistema de automatización del hogar basado en IoT para control remoto de dispositivos.",
     members: [
       { img: "/img/team-4.jpeg", name: "María Rodríguez" },
       { img: "/img/team-3.jpeg", name: "Luis Martínez" },
