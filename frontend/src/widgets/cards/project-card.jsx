71fa1780f2337dbdf488a161dc56d19f4a67fd48
Ricardo965 <ricardiox2000@gmail.com>
2024-09-25 18:05:56 -0500

    feat: homepage

--- frontend/src/widgets/cards/project-card.jsx
+++ frontend/src/widgets/cards/project-card.jsx
@@ -1,53 +1,54 @@
 import {
-    Card,
-    CardHeader,
-    CardBody,
-    CardFooter,
-    Typography,
-    Button,
-  } from "@material-tailwind/react";
-  
-  export function ProjectCard({ project, isActive }) {
-    return (
-      <Card
-        className={`transition-transform duration-300 ease-in-out ${
-          isActive ? "scale-110" : "scale-90 blur-sm"
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Typography,
+  Button,
+} from "@material-tailwind/react";
+import { Link } from "react-router-dom";
+export function ProjectCard({ project, isActive }) {
+  return (
+    <Card
+      className={`transition-transform duration-300 ease-in-out ${isActive ? "scale-110" : "scale-90 blur-sm"
         } w-96`}
-      >
-        <CardHeader shadow={false} floated={false} className="h-96">
-          <img
-            src={project.image}
-            alt="card-image"
-            className="h-full w-full object-cover"
-          />
-        </CardHeader>
-        <CardBody>
-          <div className="mb-2 flex items-center justify-between">
-            <Typography color="blue-gray" className="font-medium">
-              {project.title}
-            </Typography>
-            <Typography color="blue-gray" className="font-medium">
-              {project.price}
-            </Typography>
-          </div>
-          <Typography
-            variant="small"
-            color="gray"
-            className="font-normal opacity-75"
-          >
-            {project.description}
+    >
+      <CardHeader shadow={false} floated={false} className="h-96">
+        <img
+          src={project.img}
+          alt="card-image"
+          className="h-full w-full object-cover"
+        />
+      </CardHeader>
+      <CardBody>
+        <div className="mb-2 flex items-center justify-between">
+          <Typography color="blue-gray" className="font-medium">
+            {project.title}
           </Typography>
-        </CardBody>
-        <CardFooter className="pt-0">
+          <Typography color="blue-gray" className="font-medium">
+            {project.tag}
+          </Typography>
+        </div>
+        <Typography
+          variant="small"
+          color="gray"
+          className="font-normal opacity-75"
+        >
+          {project.description}
+        </Typography>
+      </CardBody>
+      <CardFooter className="pt-0">
+        <Link to="/project/detail/">
+
           <Button
             ripple={false}
             fullWidth={true}
             className="bg-blue-gray-900/10 text-blue-gray-900 shadow-none hover:scale-105 hover:shadow-none focus:scale-105 focus:shadow-none active:scale-100"
           >
-            Add to Cart
+            Details
           </Button>
-        </CardFooter>
-      </Card>
-    );
-  }
-  
+        </Link>
+      </CardFooter>
+    </Card>
+  );
+}
