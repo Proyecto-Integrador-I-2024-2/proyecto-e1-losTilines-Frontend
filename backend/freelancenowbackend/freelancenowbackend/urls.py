ac9fedaed5c281957d6f6d6f7813d44a9a878539
Raul Quigua <raul-quigua@hotmail.com>
2024-09-27 18:19:40 -0500

    build: users viewsets implemetation

--- backend/freelancenowbackend/freelancenowbackend/urls.py
+++ backend/freelancenowbackend/freelancenowbackend/urls.py
@@ -18,12 +18,16 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.authtoken import views as auth_views
 from appAuth.views import LoginView
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/auth/', LoginView.as_view(), name='login'),  # Login y autenticación por token
-    path('api/users/', include('appAuth.urls')),
-    path('api/profile/', include('appProfile.urls')),
-    path('api/company/', include('appCompany.urls')),
-    path('api/projects/', include('appProject.urls')),
+    path('auth/', LoginView.as_view(), name='login'),  # Login y autenticación por token
+    path('', include('appAuth.urls')),
+    path('profile/', include('appProfile.urls')),
+    path('company/', include('appCompany.urls')),
+    path('projects/', include('appProject.urls')),
 ]
