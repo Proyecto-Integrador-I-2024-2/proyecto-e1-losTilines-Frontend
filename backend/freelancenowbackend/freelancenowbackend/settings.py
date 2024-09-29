00c02480d4a2f03023303f0ef68b0da907da92d5
Sara Lucia <saludipu71@gmail.com>
2024-09-29 10:35:32 -0500

    refactor: add django-filters

--- backend/freelancenowbackend/freelancenowbackend/settings.py
+++ backend/freelancenowbackend/freelancenowbackend/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'appCompany',
     'appProject',
     'corsheaders',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -72,6 +73,10 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_FILTER_BACKENDS': 
+    [
+        'django_filters.rest_framework.DjangoFilterBackend'
     ],
 }
 
