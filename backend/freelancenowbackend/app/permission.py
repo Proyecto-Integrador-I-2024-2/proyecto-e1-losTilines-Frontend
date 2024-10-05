5eb81fca51f2b7e8e91a3a027a50f98c93e00e91
Sara Lucia <saludipu71@gmail.com>
2024-10-04 23:44:54 -0500

    refactor: permissions authentication and register app

--- backend/freelancenowbackend/app/permission.py
+++ backend/freelancenowbackend/app/permission.py
@@ -1,24 +1,21 @@
 from rest_framework.permissions import SAFE_METHODS, BasePermission
+from .models import UserCompany
 
-# Verify if the user belongs to a specific group
 class IsInGroup(BasePermission):
     group_name = None  
 
     def has_permission(self, request, view):
-        # Verify if the group_name attribute is set
         if self.group_name is None:
             raise ValueError("group_name must be defined in the subclass")
 
-        # Allows all authenticated users to perform safe methods (GET, HEAD, OPTIONS)
         if request.method in SAFE_METHODS:
             return True
 
-        # For unsafe methods (POST, PUT, PATCH, DELETE), the user must be in the specified group
         return (
             request.user
             and request.user.groups.filter(name=self.group_name).exists()
         )
-    
+
 class IsFreelancer(IsInGroup):
     group_name = "Freelancer"
 
@@ -30,3 +27,23 @@ class IsAreaAdmin(IsInGroup):
 
 class IsProjectManager(IsInGroup):
     group_name = "Project Manager"
+
+class IsOwnerOrReadOnly(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:
+            return True
+
+        return obj == request.user
+
+class IsBusinessManagerOrReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        if request.method in SAFE_METHODS:
+            return request.user and request.user.is_authenticated
+
+        return request.user.groups.filter(name='Business Manager').exists()
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:
+            return True
+
+        return UserCompany.objects.filter(user=request.user, company=obj).exists()
