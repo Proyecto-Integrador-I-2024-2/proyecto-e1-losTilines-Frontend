e50a612764ff3901ae066412e076afff994d8f0f
Raul Quigua <raul-quigua@hotmail.com>
2024-10-13 22:47:32 -0500

    feat(authApp): filter by area can be none

--- backend/freelancenowbackend/appAuth/filters.py
+++ backend/freelancenowbackend/appAuth/filters.py
@@ -1,13 +1,25 @@
 import django_filters
 from app.models import User, Company
 
-class WorkerFilter(django_filters.FilterSet):    
+class WorkerFilter(django_filters.FilterSet):
     company = django_filters.NumberFilter(field_name='usercompany__company__id')
-    area = django_filters.NumberFilter(field_name='usercompany__area__id')
+    area = django_filters.CharFilter(method='filter_by_area')
 
     class Meta:
         model = User
         fields = ['company', 'area']
+
+    def filter_by_area(self, queryset, name, value):
+        if value == 'none':
+            # Si se pasa "none" como área en la URL, filtramos por usuarios sin área
+            return queryset.filter(usercompany__area__isnull=True)
+        else:
+            # Filtramos por el ID de área si es un valor numérico
+            try:
+                area_id = int(value)
+                return queryset.filter(usercompany__area__id=area_id)
+            except ValueError:
+                return queryset
 
 class FreelancerFilter(django_filters.FilterSet):
     # Filtro por el nombre de la habilidad relacionada con el usuario
