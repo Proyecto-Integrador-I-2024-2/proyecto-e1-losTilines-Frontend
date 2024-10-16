0656a0d41faba005b46bfb4153840f097159a2e3
Ricardo965 <ricardiox2000@gmail.com>
2024-10-16 04:40:35 -0500

    feat: Experience edition and deletion

--- frontend/src/hooks/useEditFreelancerExperience.jsx
+++ frontend/src/hooks/useEditFreelancerExperience.jsx
@@ -2,14 +2,11 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import apiClient from "@/services/apiClient";
 
 // Función para realizar el PATCH a la API
-const patchWorker = async (body) => {
-    const id = sessionStorage.getItem("id");
+const patchExp = async ({ id, body }) => {
 
     // Define la ruta basada en el session storage y asegurarse del manejo adecuado de los IDs
-    var userType = "workers";
-    const url = `${userType}/${id}/`;
-
-    console.log("URL: " + url);
+    var route = "experience";
+    const url = `${route}/${id}/`;
 
     // Realiza la mutación a través de la API
     const { data } = await apiClient.patch(url, body);
@@ -17,21 +14,21 @@ const patchWorker = async (body) => {
 }
 
 // Hook que usa useMutation para editar el perfil de un trabajador
-export const useEditWorkerProfile = () => {
+export const useEditFreelancerExperience = () => {
     const queryClient = useQueryClient(); // Necesitamos el queryClient para invalidar queries
 
-    return useMutation(patchWorker, {
+    return useMutation(patchExp, {
         onSuccess: () => {
-            console.log("Perfil editado con éxito");
+            console.log("Experiencia editada con éxito");
 
             // Invalida todas las queries que tengan el ID 'Freelancer'
-            queryClient.invalidateQueries(['Freelancer', 'User']);
+            queryClient.invalidateQueries(['Freelancer']);
         },
         onError: (error) => {
-            console.error("Error al editar el perfil:", error);
+            console.error("Error al editar la exxperiencia:", error);
         },
         retry: 2,  // Reintenta la mutación 2 veces en caso de fallo
     });
 }
 
-export default useEditWorkerProfile;
+export default useEditFreelancerExperience;
