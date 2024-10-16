e5f15c5e16e0903ae42b950313c865f93b170033
Ricardo965 <ricardiox2000@gmail.com>
2024-10-16 06:13:54 -0500

    build: Freelancers listing hook

--- frontend/src/widgets/cards/freelancer-card.jsx
+++ frontend/src/widgets/cards/freelancer-card.jsx
@@ -1,44 +1,45 @@
 import {
-    Card,
-    CardHeader,
-    CardBody,
-    CardFooter,
-    Typography,
-    Tooltip,
-  } from "@material-tailwind/react";
-  
-  export function FreelancerCard({ freelancer, isActive }) {
-    return (
-      <Card className={`w-96 transition-transform duration-300 ${isActive ? "scale-105" : "scale-100"} ${isActive ? "filter-none" : "filter blur-sm"}`}>
-        <CardHeader floated={false} className="h-80">
-          <img src={freelancer.image} alt="profile-picture" className={`${isActive ? "filter-none" : "filter blur-sm"}`} />
-        </CardHeader>
-        <CardBody className="text-center">
-          <Typography variant="h4" color="blue-gray" className="mb-2">
-            {freelancer.name}
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Typography,
+  Tooltip,
+} from "@material-tailwind/react";
+
+const defaultImage = "https://i.pinimg.com/736x/f0/44/32/f044320d1fd50e8c806a2f92da661715.jpg";
+
+export function FreelancerCard({ freelancer, isActive }) {
+  return (
+    <Card className={`w-96 transition-transform duration-300 ${isActive ? "scale-105" : "scale-100"} ${isActive ? "filter-none" : "filter blur-sm"}`}>
+      <CardHeader floated={false} className="h-80">
+        <img src={freelancer.image || defaultImage} alt="profile-picture" className={`${isActive ? "filter-none" : "filter blur-sm"}`} />
+      </CardHeader>
+      <CardBody className="text-center">
+        <Typography variant="h4" color="blue-gray" className="mb-2">
+          {freelancer.name || "John Doe"}
+        </Typography>
+        <Typography color="blue-gray" className="font-medium" textGradient>
+          {freelancer.position || "Web Developer"}
+        </Typography>
+      </CardBody>
+      <CardFooter className="flex justify-center gap-7 pt-2">
+        <Tooltip content="Like">
+          <Typography as="a" href={"https://github.com/Domiciano/AppMoviles242"} variant="lead" color="blue" textGradient>
+            <i className="fab fa-facebook" />
           </Typography>
-          <Typography color="blue-gray" className="font-medium" textGradient>
-            {freelancer.position}
+        </Tooltip>
+        <Tooltip content="Follow">
+          <Typography as="a" href={"https://github.com/Domiciano/AppMoviles242"} variant="lead" color="light-blue" textGradient>
+            <i className="fab fa-twitter" />
           </Typography>
-        </CardBody>
-        <CardFooter className="flex justify-center gap-7 pt-2">
-          <Tooltip content="Like">
-            <Typography as="a" href={freelancer.facebook} variant="lead" color="blue" textGradient>
-              <i className="fab fa-facebook" />
-            </Typography>
-          </Tooltip>
-          <Tooltip content="Follow">
-            <Typography as="a" href={freelancer.twitter} variant="lead" color="light-blue" textGradient>
-              <i className="fab fa-twitter" />
-            </Typography>
-          </Tooltip>
-          <Tooltip content="Follow">
-            <Typography as="a" href={freelancer.instagram} variant="lead" color="purple" textGradient>
-              <i className="fab fa-instagram" />
-            </Typography>
-          </Tooltip>
-        </CardFooter>
-      </Card>
-    );
-  }
-  
+        </Tooltip>
+        <Tooltip content="Follow">
+          <Typography as="a" href={"https://github.com/Domiciano/AppMoviles242"} variant="lead" color="purple" textGradient>
+            <i className="fab fa-instagram" />
+          </Typography>
+        </Tooltip>
+      </CardFooter>
+    </Card>
+  );
+}
