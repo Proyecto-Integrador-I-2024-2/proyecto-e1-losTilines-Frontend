da18292126fbf07240a220da99530b8b90867436
Ricardo965 <ricardiox2000@gmail.com>
2024-10-16 06:13:54 -0500

    build: Freelancers listing hook

--- frontend/src/pages/home/homepage.jsx
+++ frontend/src/pages/home/homepage.jsx
@@ -13,6 +13,7 @@ import { CustomListItem } from "@/widgets/horList";
 import { IconButton } from "@material-tailwind/react";
 import { useProjects } from "@/hooks";
 import { useNavigate } from "react-router-dom"; // Importa useNavigate
+import { freelancersData } from "@/data";
 
 
 const Homepage = () => {
@@ -28,7 +29,7 @@ const Homepage = () => {
 
 
 
-  if (!projectsData) {
+  if (!projectsData || !freelancersData) {
     return (
       <div className="flex flex-col items-center justify-center h-full bg-gradient-to-r from-blue-500 to-purple-600 text-white">
         <h1 className="text-5xl font-bold mb-4 animate-bounce">OOPS!</h1>
@@ -49,37 +50,68 @@ const Homepage = () => {
 
 
 
-  return (
-    <div className="justify-center items-center">
-      <div className="w-full flex flex-row mt-4 px-4 justify-between">
-        <IconButton onClick={() => handleScroll(-1)}>
-          <i className="fa-solid fa-arrow-left" />
-        </IconButton>
-        <Typography variant="h3">Projects you might be interested in</Typography>
-        <IconButton onClick={() => handleScroll(1)}>
-          <i className="fa-solid fa-arrow-right" />
-        </IconButton>
-        {/* <Button  className="h-12 w-12 bg-gray-200 p-2"></Typography></Button> */}
-      </div>
-      <Card className="overscroll-y-none mx-3 mt-4 mb-2 lg:mx-4 border border-blue-gray-100">
-        <CardBody className="overscroll-y-none h-full flex flex-row">
-          <CustomList
-            sectionTitle={""}
-            sectionSubtitle={""}
-          >
-            {projectsData.map((project, index) => (
-              <ProjectCard
-                key={project.id}
-                project={project}
-                isActive={index === activeIndex}
-              />
-            ))}
-          </CustomList>
-        </CardBody>
-      </Card>
-    </div >
+  if (role === "Freelancer") {
+    return (
+      <div className="justify-center items-center">
+        <div className="w-full flex flex-row mt-4 px-4 justify-between">
+          <IconButton onClick={() => handleScroll(-1)}>
+            <i className="fa-solid fa-arrow-left" />
+          </IconButton>
+          <Typography variant="h3">Projects you might be interested in</Typography>
+          <IconButton onClick={() => handleScroll(1)}>
+            <i className="fa-solid fa-arrow-right" />
+          </IconButton>
+          {/* <Button  className="h-12 w-12 bg-gray-200 p-2"></Typography></Button> */}
+        </div>
+        <Card className="overscroll-y-none mx-3 mt-4 mb-2 lg:mx-4 border border-blue-gray-100">
+          <CardBody className="overscroll-y-none h-full flex flex-row">
+            <CustomList
+              sectionTitle={""}
+              sectionSubtitle={""}
+            >
+              {freelancersData.map((freelancer, index) => (
+                <FreelancerCard freelancer={freelancer} isActive={true} />
+              ))}
+            </CustomList>
+          </CardBody>
+        </Card>
+      </div >
+    );
+  } else {
+    return (
+      <div className="justify-center items-center">
+        <div className="w-full flex flex-row mt-4 px-4 justify-between">
+          <IconButton onClick={() => handleScroll(-1)}>
+            <i className="fa-solid fa-arrow-left" />
+          </IconButton>
+          <Typography variant="h3">Projects you might be interested in</Typography>
+          <IconButton onClick={() => handleScroll(1)}>
+            <i className="fa-solid fa-arrow-right" />
+          </IconButton>
+          {/* <Button  className="h-12 w-12 bg-gray-200 p-2"></Typography></Button> */}
+        </div>
+        <Card className="overscroll-y-none mx-3 mt-4 mb-2 lg:mx-4 border border-blue-gray-100">
+          <CardBody className="overscroll-y-none h-full flex flex-row">
+            <CustomList
+              sectionTitle={""}
+              sectionSubtitle={""}
+            >
+              {projectsData.map((project, index) => (
+                <ProjectCard
+                  key={project.id}
+                  project={project}
+                  isActive={index === activeIndex}
+                />
+              ))}
+            </CustomList>
+          </CardBody>
+        </Card>
+      </div >
+    );
+  }
 
-  );
+
+
 };
 
 export default Homepage;
