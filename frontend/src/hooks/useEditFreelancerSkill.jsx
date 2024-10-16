ce09e3e97c33a2c44010bf95a197027356a0dd30
Ricardo965 <ricardiox2000@gmail.com>
2024-10-16 04:43:37 -0500

    build: Skill deletion and edition hooks

--- frontend/src/hooks/useEditFreelancerSkill.jsx
+++ frontend/src/hooks/useEditFreelancerSkill.jsx
@@ -2,33 +2,33 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import apiClient from "@/services/apiClient";
 
 // Función para realizar el PATCH a la API
-const patchDelete = async ({ id }) => {
+const patchSkill = async ({ id, body }) => {
 
     // Define la ruta basada en el session storage y asegurarse del manejo adecuado de los IDs
-    var route = "experience";
+    var route = "freelancer-skills";
     const url = `${route}/${id}/`;
 
     // Realiza la mutación a través de la API
-    const { data } = await apiClient.delete(url);
+    const { data } = await apiClient.patch(url, body);
     return data;
 }
 
 // Hook que usa useMutation para editar el perfil de un trabajador
-export const useDeleteFreelancerExperience = () => {
+export const useEditFreelancerExperience = () => {
     const queryClient = useQueryClient(); // Necesitamos el queryClient para invalidar queries
 
-    return useMutation(patchDelete, {
+    return useMutation(patchSkill, {
         onSuccess: () => {
-            console.log("Experiencia eliminada con éxito");
+            console.log("Habilidad editada con éxito");
 
             // Invalida todas las queries que tengan el ID 'Freelancer'
             queryClient.invalidateQueries(['Freelancer']);
         },
         onError: (error) => {
-            console.error("Error al eliminar la exxperiencia:", error);
+            console.error("Error al editar la habilidad:", error);
         },
         retry: 2,  // Reintenta la mutación 2 veces en caso de fallo
     });
 }
 
-export default useDeleteFreelancerExperience;
+export default useEditFreelancerExperience;
