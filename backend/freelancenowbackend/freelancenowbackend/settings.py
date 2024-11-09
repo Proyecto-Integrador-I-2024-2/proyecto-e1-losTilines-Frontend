814cd3c525dbf12461471dbc8b549b1d03d75772
SaraLuciaaa <saludipu71@gmail.com>
2024-11-09 12:01:30 -0500

    merge(): develop updates

--- backend/freelancenowbackend/freelancenowbackend/settings.py
+++ backend/freelancenowbackend/freelancenowbackend/settings.py
@@ -199,8 +199,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'saludipu71@gmail.com'
-EMAIL_HOST_PASSWORD = 'fnhaaoagbvhbxwrg'
+EMAIL_HOST_USER = 'freelancenowmarketplace@gmail.com'
+EMAIL_HOST_PASSWORD = 'freelancenow123marketplace'
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
