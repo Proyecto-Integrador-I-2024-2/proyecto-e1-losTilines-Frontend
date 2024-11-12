fcd03fd4c25d2c34dfdfab05b291e4a7b451a090
RaulQode <raulqode@gmail.com>
2024-11-12 15:58:04 -0500

    hotfix: name

--- frontend/src/services/apiClient.jsx
+++ frontend/src/services/apiClient.jsx
@@ -1,16 +1,16 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: `${process.env.REACT_APP_BASE_URL}/`,  
+  baseURL: 'ec2-3-142-50-53.us-east-2.compute.amazonaws.com:29000/',  
   headers: {
     'Content-Type': 'application/json',
   },
 });
 
 apiClient.interceptors.request.use((config) => {
-  const token = sessionStorage.getItem('token');
+  const token = sessionStorage.getItem('token');  // Aquí obtenemos el token
   if (token) {
-    config.headers.Authorization = `Token ${token}`;
+    config.headers.Authorization = `Token ${token}`;  // Agregamos el token en las cabeceras
   }
   return config;
 });
