2966eb253f50d1e7567dd87990da2c85ed6ceb16
kira0826 <curaca492@gmail.com>
2024-11-15 16:33:16 -0500

    build: setting env vars.

---
+++ run.sh
@@ -0,0 +1,13 @@
+export POSTGRES_HOST='c3cj4hehegopde.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com'
+export POSTGRES_USER='u34sn334lgk7c1'
+export POSTGRES_PORT='5432'
+export POSTGRES_PASSWORD='pcfb18da63d70a80cf8819ba61a526a7dbe9c8724ec8fa86af3c0027ba4cdcdea'
+export POSTGRES_NAME='dbopr359hmv7vg'
+
+#Redis enviroments
+
+export REDIS_HOST='ec2-23-23-103-113.compute-1.amazonaws.com'
+export REDIS_PORT='30690'
+export REDIS_PASSWORD='p2d6633855e01a8927c1038cf906cf633822a413d8b5c1a8e87366d2cdae78709'
+export REDIS_URI='rediss://:p2d6633855e01a8927c1038cf906cf633822a413d8b5c1a8e87366d2cdae78709@ec2-23-23-103-113.compute-1.amazonaws.com:30690'
+    
